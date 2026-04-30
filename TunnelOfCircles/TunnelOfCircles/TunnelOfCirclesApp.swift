0854add5b24ff107cf22fd3da70f21b5392d2a2e
woojin <wjchoi.son@gmail.com>
2026-04-30 19:58:17 +0900

    init

---
+++ TunnelOfCircles/TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by 최우진 on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TunnelOfCircles/TunnelOfCirclesTests/TunnelOfCirclesTests.swift
@@ -0,0 +1,19 @@
+//
+//  TunnelOfCirclesTests.swift
+//  TunnelOfCirclesTests
+//
+//  Created by 최우진 on 4/30/26.
+//
+
+import Testing
+@testable import TunnelOfCircles
+
+struct TunnelOfCirclesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
